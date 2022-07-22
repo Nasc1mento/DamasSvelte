9ca16442ddd9c1b8c7d548a1ae95e42da0a15a70
Nasc1mento <reisadryan16@gmail.com>
2022-07-22 17:35:54 -0300

    criação de rotas

---
+++ my-app/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+    import Menu from "./routes/Menu.svelte";
+    import Sobre from "./routes/Sobre.svelte";
+    import Jogar from "./routes/Jogar.svelte";
+  
+    export let url = "";
+  </script>
+  
+  <Router url="{url}">
+    <nav>
+      <Link to="/">Menu</Link>
+      <Link to="sobre">Sobre</Link>
+      <Link to="jogar">Jogar</Link>
+    </nav>
+    <div>
+      <Route path="/" component="{Menu}" />
+      <Route path="jogar" component="{Jogar}" />
+      <Route path="sobre" component="{Sobre}" />
+      <Route path="/"><Menu /></Route>
+    </div>
+  </Router>
