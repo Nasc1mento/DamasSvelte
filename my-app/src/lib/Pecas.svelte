aa29d3973ef8994fe0da2b80714270aaec2f8601
Nasc1mento <reisadryan16@gmail.com>
2022-07-25 13:13:40 -0300

    movimentação das peças normais

--- my-app/src/lib/Pecas.svelte
+++ my-app/src/lib/Pecas.svelte
@@ -11,8 +11,8 @@
     
     const propriedadesTabuleiro = new Tabuleiro (8, 8);
 
-    let corJogador1 = 'blue'
-    let corJogador2 = 'red';
+    let Jogador1 = 'blue'
+    let Jogador2 = 'red';
     let tabuleiro = montarTabuleiro (propriedadesTabuleiro);
 
     function montarTabuleiro (propriedadesTabuleiro){
@@ -22,11 +22,11 @@
         for (let i = 0; i < propriedadesTabuleiro.altura; i++){
             for (let j = 0; j < propriedadesTabuleiro.largura; j++){
                 if ((i == 0 && j % 2 == 0) || (i == 1 && j % 2 == 1)){
-                    pecas[i][j] = corJogador1;
+                    pecas[i][j] = Jogador1;
                     continue;
                 }
                 if ((i == 6 && j % 2 == 0) || (i == 7 && j % 2 == 1)){
-                    pecas[i][j] = corJogador2;
+                    pecas[i][j] = Jogador2;
                     continue;
                 }
 
@@ -38,28 +38,57 @@
     
 /* --------------- Lógica do jogo* ------------------ */
 
-    let a,b, pecaEscolhida;
+    let a, b, pecaEscolhida;
 
-    function mover (x, y){
-        
-       if (pecaEscolhida == null){
+    function escolher (x, y) {
+        if (pecaEscolhida == null) {
             a = x
             b = y
             pecaEscolhida = tabuleiro[a][b]
             return;
-       }
+        }
 
-       if ((x == a + 1 || x == a -1) && (y == b + 1 || y == b -1) && tabuleiro[x][y] == null){
+       if (tabuleiro[a][b] == Jogador2) {
+            if ((x == a -1) && (y == b + 1 || y == b -1) ){
+                if (tabuleiro[x][y] == null){
+                    mover(x, y);
+                    resetar();
+                    return; 
+                }
+                
+            }
+            comer(x, y);
+            return;
+        }
+
+       if (tabuleiro[a][b] == Jogador1) {
+            if ((x == a + 1) && (y == b + 1 || y == b -1)){
+                if (tabuleiro[x][y] == null){
+                    mover(x, y);
+                    resetar(); 
+                    return; 
+                }
+            }
+
+            comer(x, y);
+            return;
+        }
+      
+    }
+
+    function comer (x, y,) {
+        
+    }
+
+    function resetar () {
+        a = null;
+        b = null;
+        pecaEscolhida = null;
+    }
+
+    function mover (x ,y) {
         tabuleiro[a][b] = null;
         tabuleiro[x][y] = pecaEscolhida;
-        pecaEscolhida = null
-        a = null
-        b = null
-       }
-
-       
-
-        
     }
 
 </script>
@@ -68,7 +97,7 @@
     {#each tabuleiro as linha, i}
         <tr>
             {#each linha as item, j}
-                <td style="background-color: {item};" class="{item != null ? 'ocupado' : 'desocupado'}" on:click={()=>mover(i, j)}></td>
+                <td style="background-color: {item};" class="{item != null ? 'ocupado' : 'desocupado'}" on:click={()=>escolher(i, j)}></td>
             {/each}
         </tr>  
     {/each}
